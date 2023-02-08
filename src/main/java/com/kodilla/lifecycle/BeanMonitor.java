5a3b1790add7423dece5c175fb537bbdc8be3dda
unknown <mateuszp.nowak@gmail.com>
2023-02-08 21:02:04 +0100

    LifeCycle

--- src/main/java/com/kodilla/lifecycle/BeanMonitor.java
+++ src/main/java/com/kodilla/lifecycle/BeanMonitor.java
@@ -1,2 +1,25 @@
-package com.kodilla.lifecycle;public class BeanMonitor {
+package com.kodilla.lifecycle;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.slf4j.LoggerFactory.*;
+public class BeanMonitor implements BeanPostProcessor {
+
+    private final Logger logger = LoggerFactory.getLogger(BeanMonitor.class);
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("Before initialization of bean: " + beanName);
+        return BeanPostProcessor.super.postProcessBeforeInitialization(bean, beanName);
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("After initialziation of bean: " + beanName);
+        logger.debug("Created bean: " + beanName);
+        return BeanPostProcessor.super.postProcessAfterInitialization(bean, beanName);
+    }
+
 }
--- src/main/java/com/kodilla/lifecycle/LifeCycleApplication.java
+++ src/main/java/com/kodilla/lifecycle/LifeCycleApplication.java
@@ -2,12 +2,19 @@ package com.kodilla.lifecycle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class LifeCycleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LifeCycleApplication.class, args);
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+		context.register(AppConfiguration.class);
+		context.refresh();
+		LibraryManager manager = context.getBean(LibraryManager.class);
+		System.out.println("Context and beans are set up and ready to work");
+		context.close();
+		//SpringApplication.run(LifeCycleApplication.class, args);
 	}
 
 }
